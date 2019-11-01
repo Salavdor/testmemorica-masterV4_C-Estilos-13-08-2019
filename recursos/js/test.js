535da7c0cd26663efde0f657b2c3b7588be58f9c
Diseño1 <Diseo1@1131MIRB01.presidencia.gob.mx>
2019-11-01 16:38:41 -0600

    Se agrega script obtenedor de objetos de AGN y pinta esos resultados en la consola

--- recursos/js/test.js
+++ recursos/js/test.js
@@ -3,7 +3,7 @@ let $nivel1 = $("#nivel1");
 
 // llena dropdown con fondos de agn
 function generateDropdown() {
-    $.getJSON("recursos/nuevoagn.json", function(data) {
+    $.getJSON("/work/models/memorica/recursos/nuevoagn.json", function(data) {
         $nivel1.append("<option disabled selected value=\"\">Selecciona un fondo</option>");
         $.each(data, function (index, value) {
             $nivel1.append("<option value=\"" + value.yo + "\">" + value.TITULO + "</option>");
@@ -12,21 +12,18 @@ function generateDropdown() {
 };
 generateDropdown();
 
-// esto en teoría va a llenar la tabla con los objetos. WIP.
+// esto en teoría va a llenar la tabla con los objetos.
 function generateTable(info) {
-    $(".COD_REF").text(info.COD_REF);
-    $(".TITULO").text(info.TITULO); // es este?
-    $(".NOMBRE_PRODUCTOR").text(info.NOMBRE_PRODUCTOR);
-    $(".NIVEL_DESC").text(info.NIVEL_DESC);
-    $(".padre").text(info.padre);
-    $(".FECHA_EXT_INI").text(info.FECHA_EXT_INI);
-    $(".HIST_INSTUTUC").text(info.HIST_INSTUTUC);
-    $(".LOCALIZ_ORIG").text(info.LOCALIZ_ORIG);
-    $(".VOL_UNID").text(info.VOL_UNID);
-    $(".FECHA_EXT_FIN").text(info.FECHA_EXT_FIN); //no, es este
-    $(".NOTAS").text(info.NOTAS);
-    $(".REGLAS_NORMAS").text(info.REGLAS_NORMAS);
-    $(".NOTA_ARCHIVERO").text(info.NOTA_ARCHIVERO);
+    // console.log(info);
+    var table = document.getElementById("gable");
+    info = JSON.parse(info);
+    var tr = document.createElement("tr");
+        tr.innerHTML = "<td>" + info.data._id + "</td>" + 
+            "<td>" + info.data.name + "</td>" + 
+            "<td>" + info.data.alternate_name + "</td>" + 
+            "<td>" + info.data.description + "</td>";
+        table.appendChild(tr);
+    });
 };
 
 // esto escribe la descripción del fondo
@@ -39,18 +36,25 @@ $("#nivel1").change(function () {
     var key = $(this).val();
 
     // añade el valor de select al url
-    // window.location.hash = "?param=" + key;
-    window.location.hash = key;
+    var pageUrl = "?param=" + key;
+    window.history.pushState("", "", pageUrl);
     
     // switch de casos de dropdown
     switch (key) {
         case key:
-            $.getJSON("recursos/nuevoagn.json", function (data) {
+            $.getJSON("/work/models/memorica/recursos/nuevoagn.json", function (data) {
                 // selecciona objeto dentro de arreglo
                 var fondo = data.find(function (v) {
                     return v.yo === key;
                 });
-                console.log(fondo);
+
+                // obtiene objetos del agn y los imprime
+                acervos.getData(key, true, function (data) {
+                    generateTable(data)
+                    // console.log(data)
+                }, function err(e) {
+                    console.log(e)
+                });
 
                 // genera descripción de acervo
                 generateDescription(fondo);
